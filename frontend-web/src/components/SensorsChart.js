fe277d6403ce22da368247b8b6ee0a6f35d4c5e7
Giacomo Fornari <fornarigiacomo@gmail.com>
2018-01-19 18:35:01 +0100

    Add random live data

--- frontend-web/src/components/SensorsChart.js
+++ frontend-web/src/components/SensorsChart.js
@@ -6,9 +6,16 @@ class SensorsChart {
       chart: {
         defaultSeriesType: 'spline',
         // useful with live data
-        // events: {
-        //     load: requestData
-        // }
+        events: {
+            load: function() {
+              setInterval(() => {
+                this.series.forEach((serie) => serie.addPoint(
+                  [(new Date()).getTime(), Math.round(Math.random() * 1000) / 100],
+                  true, true, true
+                ));
+              }, 3000);
+            }
+        }
       },
       title: {
         text: `Sensors measurements of bulding ${buildingId}`,
@@ -30,13 +37,13 @@ class SensorsChart {
         name: 'Sensor1',
         // just a bad way to generate random data
         data: [...new Array(10)].map((v, i) => [
-          (new Date()).getTime() + i * 10000,
+          (new Date()).getTime() - (9 - i) * 3000,
           Math.round(Math.random() * 1000) / 100
         ])
       }, {
         name: 'Sensor2',
         data: [...new Array(10)].map((v, i) => [
-          (new Date()).getTime() + i * 10000,
+          (new Date()).getTime() - (9 - i) * 3000,
           Math.round(Math.random() * 1000) / 100
         ])
       }]
