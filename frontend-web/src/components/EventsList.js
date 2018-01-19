8fa537200b03cdd2e0e0d94335235f946d878cd4
Giacomo Fornari <fornarigiacomo@gmail.com>
2018-01-19 20:15:15 +0100

    Add some layout

--- frontend-web/src/components/EventsList.js
+++ frontend-web/src/components/EventsList.js
@@ -7,15 +7,15 @@ class EventsList {
 
   appendItem(type, text, secondaryText) {
     const strElement = `
-      <li class="mdc-list-item" data-mdc-auto-init="MDCRipple">
-      <span class="mdc-list-item__graphic" role="presentation">
-        <i class="material-icons" aria-hidden="true">${type}</i>
-      </span>
-      <span class="mdc-list-item__text">
-        ${text}
-        <span class="mdc-list-item__secondary-text">${secondaryText}</span>
-      </span>
-    </li>`;
+      <li class="mdc-list-item" role="listitem" data-mdc-auto-init="MDCRipple">
+        <span class="mdc-list-item__graphic" role="presentation">
+          <i class="material-icons" aria-hidden="true">${type}</i>
+        </span>
+        <span class="mdc-list-item__text">
+          ${text}
+          <span class="mdc-list-item__secondary-text">${secondaryText}</span>
+        </span>
+      </li>`;
     this.container.querySelector('ul').insertAdjacentHTML('beforeend', strElement);
   }
 }
