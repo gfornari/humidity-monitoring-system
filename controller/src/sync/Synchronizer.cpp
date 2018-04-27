d113c5f58b63d4d2e211193521cfd762347c9c03
Giacomo Fornari <fornarigiacomo@gmail.com>
2018-04-27 12:06:13 +0200

    Add Synchronizer class

---
+++ controller/src/sync/Synchronizer.cpp
@@ -0,0 +1,14 @@
+#include "Synchronizer.h"
+
+void Synchronizer::sync() {
+  // see if there are scheduled jobs
+
+  // send scheduled jobs
+
+  // get most recent firebase doc
+
+  // get all docs younger than firebase doc
+
+  // if there are docs, send all docs retrived
+    // if someone failed, reschedule them
+}
---
+++ controller/src/sync/Synchronizer.h
@@ -0,0 +1,15 @@
+#ifndef SYNCHRONIZER_H
+#define SYNCHRONIZER_H
+
+#include <string>
+
+// #include <Poco/JSON/Object.h>
+
+class Synchronizer {
+  public:
+    Synchronizer();
+    void sync();
+};
+
+
+#endif //SYNCHRONIZER_H
